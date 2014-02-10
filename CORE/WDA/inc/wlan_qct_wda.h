584a8f700532547facf48fdd354e42d1f191d2c8
Sandeep Puligilla <spuligil@qti.qualcomm.com>
2014-02-10 16:39:05 +0530

    wlan:WDA:Support of OBSS Scan on 2.4GHz
    - To support HT40 on 2.4GHz,STA should perform the
    OBSS scan and send the TE-A and TE-B events at end of the scan.
    - OBSS scan is currently handled at the firmware.
    - After successful connection on 2.4GHz channel in HT40 mode
      OBSS scan will be initated by OBSS SCAN indication to firmware.
    - OBSS scan will be stopped by firmware during the disconnection.
    - Added OBSS scan stop indication for debugging purpose.
    - This gerit takes care of WDI/WDA implementation.
    
    CRs-Fixed: 608188
    Change-Id: I2671d9b8088ec3376c01ad1518967d9cf934af4a

--- CORE/WDA/inc/wlan_qct_wda.h
+++ CORE/WDA/inc/wlan_qct_wda.h
@@ -1116,6 +1116,8 @@ tSirRetStatus uMacPostCtrlMsg(void* pSirGlobal, tSirMbMsg* pMb);
 #define WDA_TRIGGER_BATCH_SCAN_RESULT_IND SIR_HAL_TRIGGER_BATCH_SCAN_RESULT_IND
 #endif
 
+#define WDA_HT40_OBSS_SCAN_IND   SIR_HAL_HT40_OBSS_SCAN_IND
+#define WDA_HT40_OBSS_STOP_SCAN_IND SIR_HAL_HT40_OBSS_STOP_SCAN_IND
 tSirRetStatus wdaPostCtrlMsg(tpAniSirGlobal pMac, tSirMsgQ *pMsg);
 
 eHalStatus WDA_SetRegDomain(void * clientCtxt, v_REGDOMAIN_t regId,
