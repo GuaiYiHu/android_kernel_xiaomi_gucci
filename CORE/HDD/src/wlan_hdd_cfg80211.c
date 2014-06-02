35e71e7e40b89d6d594f38e4a9ce7da2d2bb82e9
Hanumantha Reddy Pothula <c_hpothu@codeaurora.org>
2014-06-02 18:01:50 +0530

    wlan: Fix for redundant TDLS log during scan_done callback.
    In wlan_hdd_tdls_check_bmps(), NULL check for pHddTdlsCtx will be
    TRUE if TDLS support is not enabled. This leads redundant logs,
    as this is called for every scan_done callback.
    Call wlan_hdd_tdls_scan_done_callback() only when TDLS support is
    enabled.
    
    Change-Id: I02d6c3fd7d5f2355386fed6150098bba7fc7e0e9
    CRs-Fixed: 672099

--- CORE/HDD/src/wlan_hdd_cfg80211.c
+++ CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -4841,7 +4841,10 @@ allow_suspend:
     hdd_prevent_suspend_timeout(1000);
 
 #ifdef FEATURE_WLAN_TDLS
-    wlan_hdd_tdls_scan_done_callback(pAdapter);
+    if (!(eTDLS_SUPPORT_NOT_ENABLED == pHddCtx->tdls_mode))
+    {
+        wlan_hdd_tdls_scan_done_callback(pAdapter);
+    }
 #endif
 
     EXIT();
