8d94d9accf94dbdac21ff2839ffb0e3872e4a19c
guaiyihu <guaiyihu@foxmail.com>
2018-07-20 10:41:32 +0800

    sound: Import xiaomi changes
    Change-Id: Iafc664c09ac99d8cb06485b673dfebdc07196738

--- sound/soc/msm/msm8x16.c
+++ sound/soc/msm/msm8x16.c
@@ -1069,15 +1069,15 @@ static void *def_msm8x16_wcd_mbhc_cal(void)
 	btn_high = btn_cfg->_v_btn_high;
 
 	btn_low[0] = 0;
-	btn_high[0] = 25;
-	btn_low[1] = 25;
-	btn_high[1] = 50;
-	btn_low[2] = 50;
-	btn_high[2] = 75;
-	btn_low[3] = 75;
-	btn_high[3] = 112;
-	btn_low[4] = 112;
-	btn_high[4] = 137;
+	btn_high[0] = 50;
+	btn_low[1] = 50;
+	btn_high[1] = 200;
+	btn_low[2] = 200;
+	btn_high[2] = 500;
+	btn_low[3] = 500;
+	btn_high[3] = 512;
+	btn_low[4] = 512;
+	btn_high[4] = 524;
 
 	return msm8x16_wcd_cal;
 }
